9cab3ae64a8585a327a99f95e09e9a709fc7074d
PradeepCodes <pradeepcse1998@gmail.com>
2025-05-04 04:44:09 +0530

    First Commits

--- src/Basic_Java_01_Helloworld.java
+++ src/Basic_Java_01_Helloworld.java
@@ -1,5 +1,5 @@
 public class Basic_Java_01_Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Worlda");
     }
 }
